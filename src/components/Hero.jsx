f6e0799dd4924a57ef3976362594c39c778a4d02
devsargam <sargampoudel100@gmail.com>
2022-06-25 15:27:29 +0545

    convert class to className

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,20 +5,16 @@ import useTyping from "../hooks/useTyping";
 function Hero() {
   let intro = useTyping("It's me Sargam Poudel.");
   return (
-    <div class="hero min-h-screen bg-base-200">
-      <div class="hero-content flex-col lg:flex-row">
-        <img
-          //   src="https://api.lorem.space/image/movie?w=260&h=400"
-          src={dp}
-          class="max-w-sm rounded-lg shadow-2xl"
-        />
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <img src={dp} className="max-w-sm rounded-lg shadow-2xl" />
         <div>
-          <h1 class="text-5xl font-bold"> {intro}</h1>
-          <p class="py-6">
+          <h1 className="text-5xl font-bold"> {intro}</h1>
+          <p className="py-6">
             Full stack web developer based in Nepal who likes to get things done
             with javascript and python.
           </p>
-          <button class="btn btn-primary">Download my résumé</button>
+          <button className="btn btn-primary">Download my résumé</button>
         </div>
       </div>
     </div>
